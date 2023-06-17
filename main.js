2bf9a8971f988011a2340d188318a09437a8dca7
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-06-17 20:18:19 -0300

    enviando exercicio sobre es6

--- main.js
+++ main.js
@@ -1,44 +1,28 @@
-function Pessoa(nome) {
-    this.nome = nome;
-}
+const alunos = [
+    {
+        nome: 'paulinho',
+        nota:10,
+    },
+        {
+        nome: 'maria',
+        nota:5,
+    },
+        {
+        nome: 'zezinho',
+        nota:7,
+    },
+        {
+        nome: 'aninha',
+        nota:3,
+    },
+]
 
-function Jogador (nome, posicao, time, salario) {
-    Pessoa.call(this, nome)
-    this.posicao = posicao,
-    this.time = time
-    let _salario = salario;
 
-    this.getSalario = function() {
-        return _salario;
-    }
-
-    this.setSalario = function(valor) {
-        if(typeof valor === 'number') {
-            _salario = valor
-        }
-    }
-
+function Aprovados(alunos) {
+    return alunos.nota >= 6
     
 }
 
-function Atacante (nome) {
-    Jogador.call(this, nome, 'atacante', 'corinthians', 15000)
-}
+const alunosFiltrados = alunos.filter(Aprovados)
 
-function Goleiro (nome) {
-    Jogador.call(this, nome, 'Goleiro', 'corinthians', 8000)
-}
-
-const jogador1 = new Jogador('joaozinho', 'atacante', 'corinthians', 13000)
-
-const jogador2 = new Atacante('pedrinho')
-
-const jogador3 = new Goleiro ('zezinho')
-
-console.log(jogador1)
-console.log(jogador2)
-console.log(jogador3)
-
-console.log(jogador1.getSalario())
-console.log(jogador2.getSalario())
-console.log(jogador3.getSalario())
+console.log(alunosFiltrados)
