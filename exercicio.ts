368d50e2c207094c1345b727c59274f42eef314e
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-06-18 00:18:00 -0300

    enviando exercicio de typescript

---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao (a: number, b: number): number {
+    return a * b
+}
+
+console.log(multiplicacao(12,23))
+
+function dizOla (nome: string):string {
+    return 'Olá ' + nome;
+}
+
+console.log(dizOla('talisson'))
